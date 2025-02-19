e1822262e5a3aa24ed60ec8bebedcdb75b7ae17e
AlondraHackerBae <khrissy921@gmail.com>
2025-02-19 13:40:24 -0600

    added contact links

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">Weather Forecast</div>
-  )
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://meetalondra.netlify.app/" target="_blank">
+          Alondra Turner{" "}
+        </a>
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/AlondraHackerBae/react-weather-forecast"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
