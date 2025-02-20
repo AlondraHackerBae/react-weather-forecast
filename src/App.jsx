2b8a65dd8a6ae2b3b14b73a4a5ab1f0452a7135e
AlondraHackerBae <khrissy921@gmail.com>
2025-02-19 22:46:19 -0600

    added css to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+import Weather from "./Weather";
+import "./Weather.css";
+import "./index.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://meetalondra.netlify.app/" target="_blank">
-          Alondra Turner{" "}
-        </a>
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/AlondraHackerBae/react-weather-forecast"
-          target="_blank"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a href="https://meetalondra.netlify.app/" target="_blank">
+            Alondra Turner{" "}
+          </a>
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/AlondraHackerBae/react-weather-forecast"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city ..."
+              className="form-control"
+              autofocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-secondary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>Houston, Tx USA</h1>
+      <ul>
+        <li>Wednesday 1:00pm</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAg5JREFUeNrt2tGRgyAQBmBLoARLSAmUkBIsISVYgiVYgiVQgq//myXQAfdwS4bJqRFuCWjWmZ3JaGYin8AumMY513xzNAIgAAIgAAIgAAIgAAIgAALwe4LhANACGADMANxLzHStbQoc2QGoce5g9JcBAKA2nvi7mAGoUwP8o/FPhOoBAGgAIwBD0ftxHNntiw6HJICdBiqa8BxD2E9MjNEAAO4bNzwxPn0fA/WsbNkjBWDZuNkHXZ8ZAQ5DfQQAwG3nJjR9xxWKpOwRC6D3AGgOcGdCYAMI0p8rHEMWAGq82Zit++B7roJoWQEAdBs/NPouR/PDUAnAwAaw0+27YKEzVdLw6EryCIDZabymIeBqC06AP93+QEa4LMASlLv2GwF81zc1N54bwJe29kA1WHMY/wBjAe4vY388KUBYLbY5FkNniudSOwXAXSTmlLWAKtRl/a4T91K7qxlgXKvpKQVzzUOmxiFgg70FBeBBu0I9fVacVWiNk6AOFmB2A6gLUrIO9wAoa5mcADnT4Phm9blWlClC0B6Pzve5AHIWQm1EmW1pSNiVcv129GGlboubXGmJGsWS56l37GFOqQA5FkMmputGDKeRLQ1+4mAEWKp4OxzR6KXEirA4QOZ9RPPuHWNRAMb3iMmbpKUBdOkNktIAt+oB5F9iAiAAAiAAAiAAAiAAAiAAAiAAAiAAl48fFVnRpiVnD+AAAAAASUVORK5CYII="
+            alt="cloudy"
+          />
+          <span className="weather-temp">20</span>
+          <span className="weather-unit">°C</span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation 17%</li>
+            <li>Humidity 30%</li>
+            <li>Wind: 13mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
